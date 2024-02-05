7a8ee7b3a472296fe4069dffa4c548449284411a
Linux dsa_shua <xyannide04@gmail.com>
2024-02-05 14:00:12 +0900

    Working systolic array!

--- src/top_module.sv
+++ src/top_module.sv
@@ -1,31 +1,17 @@
-/*
-
-    TODO: instead of a block of memory, 
-
-    have multiple array for each register sequence.
-
-    when writing, take the index, and the register number.
-
-
-    // Follow SystolicArray_tb.sv PLEASE!!!
-
-*/
-
-
 module top_module(
 
 
     input CLK,
     input EN,
-    // input RF_EN,                // can be removed.
 
     input WRITE,                    // WRITE TO INT_MEM
-    // input READ,
+    
 
+    input [2:0] IDX,                // index of the element
+    input [3:0] REG,                // which register
 
     input LOAD,                     // Load to REGISTER FILE
     
-    input [7:0]  ADDR,
     input [15:0] DATA_IN,
 
     output [15:0] DATA_OUT
@@ -35,7 +21,7 @@ module top_module(
 
     //                      SYSTOLIC ARRAY
 
-    reg [4:0]   IDX = 0;
+    // reg [4:0]   IDX = 0;
     
     reg [15:0]  DATA_0 = 0;
     reg [15:0]  DATA_1 = 0;
@@ -54,8 +40,6 @@ module top_module(
     reg [15:0]  DATA_14 = 0;
     reg [15:0]  DATA_15 = 0;
 
-    
-    reg [3:0]   REG_SELECT;
 
     /////////////////////////////////////////////////////////////
     
@@ -64,69 +48,86 @@ module top_module(
 
     reg [15:0] DATA_OUT_BUFFER = 0;
 
-    // Internal Memory
-    reg [15:0] INT_MEM [0:255];
-
     // Matrix 1
-    reg [15:0] SEQ_0 [0:7];
-    reg [15:0] SEQ_1 [0:7];
-    reg [15:0] SEQ_2 [0:7];
-    reg [15:0] SEQ_3 [0:7];
-    reg [15:0] SEQ_4 [0:7];
-    reg [15:0] SEQ_5 [0:7];
-    reg [15:0] SEQ_6 [0:7];
-    reg [15:0] SEQ_7 [0:7];
+    reg [15:0] MAT_A0 [0:7];
+    reg [15:0] MAT_A1 [0:7];
+    reg [15:0] MAT_A2 [0:7];
+    reg [15:0] MAT_A3 [0:7];
+    reg [15:0] MAT_A4 [0:7];
+    reg [15:0] MAT_A5 [0:7];
+    reg [15:0] MAT_A6 [0:7];
+    reg [15:0] MAT_A7 [0:7];
     
+
     // Matrix 2
-    reg [15:0] SEQ_8 [0:7];
-    reg [15:0] SEQ_9 [0:7];
-    reg [15:0] SEQ_A [0:7];
-    reg [15:0] SEQ_B [0:7];
-    reg [15:0] SEQ_C [0:7];
-    reg [15:0] SEQ_D [0:7];
-    reg [15:0] SEQ_E [0:7];
-    reg [15:0] SEQ_F [0:7];
-
-    initial begin
-        for (integer i = 0; i < 256; i = i + 1) begin
-            INT_MEM[i[7:0]] = 16'b0;
-        end
-    end
+    reg [15:0] MAT_B0 [0:7];
+    reg [15:0] MAT_B1 [0:7];
+    reg [15:0] MAT_B2 [0:7];
+    reg [15:0] MAT_B3 [0:7];
+    reg [15:0] MAT_B4 [0:7];
+    reg [15:0] MAT_B5 [0:7];
+    reg [15:0] MAT_B6 [0:7];
+    reg [15:0] MAT_B7 [0:7];
 
 
-    reg [31:0] IDX_COUNTER = 0;
+    // Result Matrix
+    reg [15:0] MAT_C0 [0:7];
+    reg [15:0] MAT_C1 [0:7];
+    reg [15:0] MAT_C2 [0:7];
+    reg [15:0] MAT_C3 [0:7];
+    reg [15:0] MAT_C4 [0:7];
+    reg [15:0] MAT_C5 [0:7];
+    reg [15:0] MAT_C6 [0:7];
+    reg [15:0] MAT_C7 [0:7];
+
+
+    reg [2:0] IDX_COUNTER = 0;
+    reg [2:0] IDX_IN_BUFFER = 0;
+    reg [15:0] DATA_IN_BUFFER = 0;
 
     reg SA_EN = 0;
     reg SA_WRITE = 0;
 
   
+    always_comb begin
+        
+    end
   
     always@(posedge CLK) begin
         if(EN) begin
+            // Always connect results register!
+
+
+
 
             if(LOAD) begin
 
                 // Load to registers (constant addresses)
                 if (!WRITE) begin
+                    SA_EN <= 1;
+                    SA_WRITE <= 1;
 
-                    DATA_0 <= SEQ_0[IDX_COUNTER];
-                    DATA_1 <= SEQ_1[IDX_COUNTER];
-                    DATA_2 <= SEQ_2[IDX_COUNTER];
-                    DATA_3 <= SEQ_3[IDX_COUNTER];
-                    DATA_4 <= SEQ_4[IDX_COUNTER];
-                    DATA_5 <= SEQ_5[IDX_COUNTER];
-                    DATA_6 <= SEQ_6[IDX_COUNTER];
-                    DATA_7 <= SEQ_7[IDX_COUNTER];
-                    DATA_8 <= SEQ_8[IDX_COUNTER];
-                    DATA_9 <= SEQ_9[IDX_COUNTER];
-                    DATA_10 <= SEQ_A[IDX_COUNTER];
-                    DATA_11 <= SEQ_B[IDX_COUNTER];
-                    DATA_12 <= SEQ_C[IDX_COUNTER];
-                    DATA_13 <= SEQ_D[IDX_COUNTER];
-                    DATA_14 <= SEQ_E[IDX_COUNTER];
-                    DATA_15 <= SEQ_F[IDX_COUNTER];
+                    DATA_0 <= MAT_A0[IDX_COUNTER];
+                    DATA_1 <= MAT_A1[IDX_COUNTER];
+                    DATA_2 <= MAT_A2[IDX_COUNTER];
+                    DATA_3 <= MAT_A3[IDX_COUNTER];
+                    DATA_4 <= MAT_A4[IDX_COUNTER];
+                    DATA_5 <= MAT_A5[IDX_COUNTER];
+                    DATA_6 <= MAT_A6[IDX_COUNTER];
+                    DATA_7 <= MAT_A7[IDX_COUNTER];
+
+                    DATA_8 <= MAT_B0[IDX_COUNTER];
+                    DATA_9 <= MAT_B1[IDX_COUNTER];
+                    DATA_10 <= MAT_B2[IDX_COUNTER];
+                    DATA_11 <= MAT_B3[IDX_COUNTER];
+                    DATA_12 <= MAT_B4[IDX_COUNTER];
+                    DATA_13 <= MAT_B5[IDX_COUNTER];
+                    DATA_14 <= MAT_B6[IDX_COUNTER];
+                    DATA_15 <= MAT_B7[IDX_COUNTER];
+                    
+
                     IDX_COUNTER <= IDX_COUNTER + 1;
-
+                    IDX_IN_BUFFER <= IDX_COUNTER;                    
                 end
 
                 // LOAD && WRITE == READ!
@@ -134,9 +135,17 @@ module top_module(
                     SA_EN <= 0;
                     SA_WRITE <= 0;
 
-                    DATA_OUT_BUFFER <= INT_MEM[ADDR];
+                    case (REG) 
+                        0: DATA_OUT_BUFFER <= MAT_C0[IDX];
+                        1: DATA_OUT_BUFFER <= MAT_C1[IDX];
+                        2: DATA_OUT_BUFFER <= MAT_C2[IDX];
+                        3: DATA_OUT_BUFFER <= MAT_C3[IDX];
+                        4: DATA_OUT_BUFFER <= MAT_C4[IDX];
+                        5: DATA_OUT_BUFFER <= MAT_C5[IDX];
+                        6: DATA_OUT_BUFFER <= MAT_C6[IDX];
+                        7: DATA_OUT_BUFFER <= MAT_C7[IDX];
+                    endcase
                 end
-
             end
 
             
@@ -144,12 +153,26 @@ module top_module(
             // If not loading, then 
             else if(!LOAD) begin
                 
-                // Writing to INT_MEM
+                // Writing to Seqeunce Registers
                 if(WRITE) begin
-                    SA_EN <= 0;
-                    SA_WRITE <= 0;
-
-                    INT_MEM[ADDR] <= DATA_IN;
+                    case (REG)
+                        4'd0 : MAT_A0[IDX] <= DATA_IN;
+                        4'd1 : MAT_A1[IDX] <= DATA_IN;
+                        4'd2 : MAT_A2[IDX] <= DATA_IN;
+                        4'd3 : MAT_A3[IDX] <= DATA_IN;
+                        4'd4 : MAT_A4[IDX] <= DATA_IN;
+                        4'd5 : MAT_A5[IDX] <= DATA_IN;
+                        4'd6 : MAT_A6[IDX] <= DATA_IN;
+                        4'd7 : MAT_A7[IDX] <= DATA_IN;
+                        4'd8 : MAT_B0[IDX] <= DATA_IN;
+                        4'd9 : MAT_B1[IDX] <= DATA_IN;
+                        4'hA : MAT_B2[IDX] <= DATA_IN;
+                        4'hB : MAT_B3[IDX] <= DATA_IN;
+                        4'hC : MAT_B4[IDX] <= DATA_IN;
+                        4'hD : MAT_B5[IDX] <= DATA_IN;
+                        4'hE : MAT_B6[IDX] <= DATA_IN;
+                        4'hF : MAT_B7[IDX] <= DATA_IN;
+                    endcase
                 end
 
                 
@@ -166,12 +189,12 @@ module top_module(
 
     ///////////////////////////////////////////////////////////////
 
-
+    // Only when !WRITE (!SA_WRITE), will the matmul begin
         
     SystolicArray sa_module(
-        .CLK(CLK), .EN(SA_EN), .RF_EN(SA_EN), .WRITE(SA_WRITE), .IDX(IDX), 
+        .CLK(CLK), .EN(SA_EN), .RF_EN(SA_EN), .WRITE(SA_WRITE), .IDX(IDX_IN_BUFFER), 
         
-        
+        // Inputs to registers
         .DIN_0(DATA_0), 
         .DIN_1(DATA_1), 
         .DIN_2(DATA_2), 
@@ -187,10 +210,18 @@ module top_module(
         .DIN_12(DATA_12), 
         .DIN_13(DATA_13), 
         .DIN_14(DATA_14), 
-        .DIN_15(DATA_15), 
-        
-        
-        .REG_SELECT(REG_SELECT)
+        .DIN_15(DATA_15),
+
+        // Systolic Array Outputs
+        .Y_00(MAT_C0[0]), .Y_10(MAT_C0[1]), .Y_20(MAT_C0[2]), .Y_30(MAT_C0[3]), .Y_40(MAT_C0[4]), .Y_50(MAT_C0[5]), .Y_60(MAT_C0[6]), .Y_70(MAT_C0[7]),
+        .Y_01(MAT_C1[0]), .Y_11(MAT_C1[1]), .Y_21(MAT_C1[2]), .Y_31(MAT_C1[3]), .Y_41(MAT_C1[4]), .Y_51(MAT_C1[5]), .Y_61(MAT_C1[6]), .Y_71(MAT_C1[7]),
+        .Y_02(MAT_C2[0]), .Y_12(MAT_C2[1]), .Y_22(MAT_C2[2]), .Y_32(MAT_C2[3]), .Y_42(MAT_C2[4]), .Y_52(MAT_C2[5]), .Y_62(MAT_C2[6]), .Y_72(MAT_C2[7]),
+        .Y_03(MAT_C3[0]), .Y_13(MAT_C3[1]), .Y_23(MAT_C3[2]), .Y_33(MAT_C3[3]), .Y_43(MAT_C3[4]), .Y_53(MAT_C3[5]), .Y_63(MAT_C3[6]), .Y_73(MAT_C3[7]),
+        .Y_04(MAT_C4[0]), .Y_14(MAT_C4[1]), .Y_24(MAT_C4[2]), .Y_34(MAT_C4[3]), .Y_44(MAT_C4[4]), .Y_54(MAT_C4[5]), .Y_64(MAT_C4[6]), .Y_74(MAT_C4[7]),
+        .Y_05(MAT_C5[0]), .Y_15(MAT_C5[1]), .Y_25(MAT_C5[2]), .Y_35(MAT_C5[3]), .Y_45(MAT_C5[4]), .Y_55(MAT_C5[5]), .Y_65(MAT_C5[6]), .Y_75(MAT_C5[7]),
+        .Y_06(MAT_C6[0]), .Y_16(MAT_C6[1]), .Y_26(MAT_C6[2]), .Y_36(MAT_C6[3]), .Y_46(MAT_C6[4]), .Y_56(MAT_C6[5]), .Y_66(MAT_C6[6]), .Y_76(MAT_C6[7]),
+        .Y_07(MAT_C7[0]), .Y_17(MAT_C7[1]), .Y_27(MAT_C7[2]), .Y_37(MAT_C7[3]), .Y_47(MAT_C7[4]), .Y_57(MAT_C7[5]), .Y_67(MAT_C7[6]), .Y_77(MAT_C7[7])
+
     );
 
     assign DATA_OUT = DATA_OUT_BUFFER;
--- src/top_module_tb.sv
+++ src/top_module_tb.sv
@@ -4,13 +4,78 @@ module top_module_tb();
     reg EN;
     reg WRITE;
     reg LOAD;
-
-    reg [7:0] ADDR;
+    
+    wire [15:0] DATA_OUT;
     reg [15:0] DATA_IN;
 
-    reg [15:0] DATA_OUT;
+    reg [2:0] IDX_SELECT;
+    reg [3:0] REG_SELECT;
 
-    wire [15:0] DATA_OUT;
+/*
+    // TEST 1
+    reg [15:0] EXTERNAL_DATA_0 [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_1 [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_2 [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_3 [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_4 [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_5 [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_6 [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_7 [0:7] = {1,2,3,4,5,6,7,8};
+
+
+    // Transposed Weights Matrix
+    reg [15:0] EXTERNAL_DATA_8 [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_9 [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_A [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_B [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_C [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_D [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_E [0:7] = {1,2,3,4,5,6,7,8};
+    reg [15:0] EXTERNAL_DATA_F [0:7] = {1,2,3,4,5,6,7,8};
+*/
+
+
+
+////////////////////////////////////////////////////////////////////
+
+    // TEST 2
+
+    reg [15:0] EXTERNAL_DATA_0 [0:7] = {0, 2, 3, 1, 0, 1, 2, 3};
+    reg [15:0] EXTERNAL_DATA_1 [0:7] = {0, 2, 0, 3, 1, 1, 4, 4};
+    reg [15:0] EXTERNAL_DATA_2 [0:7] = {1, 2, 4, 4, 2, 4, 3, 0};
+    reg [15:0] EXTERNAL_DATA_3 [0:7] = {0, 2, 4, 2, 2, 0, 4, 2};
+    reg [15:0] EXTERNAL_DATA_4 [0:7] = {2, 2, 0, 0, 4, 4, 3, 1};
+    reg [15:0] EXTERNAL_DATA_5 [0:7] = {2, 4, 4, 0, 2, 0, 0, 3};
+    reg [15:0] EXTERNAL_DATA_6 [0:7] = {4, 1, 4, 1, 3, 3, 3, 3};
+    reg [15:0] EXTERNAL_DATA_7 [0:7] = {2, 4, 1, 4, 4, 2, 3, 4};
+
+
+    // Transposed Weights Matrix
+    reg [15:0] EXTERNAL_DATA_8 [0:7] = {4, 0, 3, 1, 0, 1, 1, 1};
+    reg [15:0] EXTERNAL_DATA_9 [0:7] = {3, 3, 4, 4, 1, 0, 3, 2};
+    reg [15:0] EXTERNAL_DATA_A [0:7] = {4, 4, 1, 1, 1, 2, 0, 4};
+    reg [15:0] EXTERNAL_DATA_B [0:7] = {3, 2, 3, 1, 0, 1, 0, 4};
+    reg [15:0] EXTERNAL_DATA_C [0:7] = {0, 3, 2, 1, 2, 2, 0, 4};
+    reg [15:0] EXTERNAL_DATA_D [0:7] = {4, 1, 0, 4, 3, 2, 1, 1};
+    reg [15:0] EXTERNAL_DATA_E [0:7] = {1, 2, 1, 2, 4, 1, 1, 0};
+    reg [15:0] EXTERNAL_DATA_F [0:7] = {2, 3, 1, 1, 0, 2, 1, 4};
+
+
+    /*
+    RESULT:
+    16, 34, 26, 27, 27, 13, 12, 26 
+    12, 39, 30, 24, 29, 27, 19, 31 
+    27, 52, 30, 27, 30, 39, 32, 27 
+    20, 48, 24, 26, 28, 22, 24, 24 
+    16, 27, 32, 18, 26, 34, 29, 25 
+    23, 42, 42, 38, 36, 21, 22, 32 
+    38, 53, 46, 42, 36, 42, 30, 37 
+    24, 59, 53, 39, 46, 51, 40, 44
+        
+    
+    */
+
+////////////////////////////////////////////////////////////////////
 
 
     initial begin
@@ -24,103 +89,73 @@ module top_module_tb();
         WRITE = 1;
         LOAD = 0;
 
-        // Elements
-        for(integer i = 0; i < 64; i = i + 1) begin
-            DATA_IN = (i[15:0] % 8) + 1;
-            ADDR = i[7:0];
+        for(integer register = 0; register < 16; register = register + 1) begin
+            REG_SELECT = register[3:0];
+            for(integer element = 0; element < 8; element = element + 1) begin
+                IDX_SELECT = element[2:0];
 
-            $display("DATA_IN: %d | ADDR: %d", DATA_IN, ADDR);
+                case (REG_SELECT)
+                    0: DATA_IN = EXTERNAL_DATA_0[IDX_SELECT];
+                    1: DATA_IN = EXTERNAL_DATA_1[IDX_SELECT];
+                    2: DATA_IN = EXTERNAL_DATA_2[IDX_SELECT];
+                    3: DATA_IN = EXTERNAL_DATA_3[IDX_SELECT];
+                    4: DATA_IN = EXTERNAL_DATA_4[IDX_SELECT];
+                    5: DATA_IN = EXTERNAL_DATA_5[IDX_SELECT];
+                    6: DATA_IN = EXTERNAL_DATA_6[IDX_SELECT];
+                    7: DATA_IN = EXTERNAL_DATA_7[IDX_SELECT];
+                    8: DATA_IN = EXTERNAL_DATA_8[IDX_SELECT];
+                    9: DATA_IN = EXTERNAL_DATA_9[IDX_SELECT];
+                    10: DATA_IN = EXTERNAL_DATA_A[IDX_SELECT];
+                    11: DATA_IN = EXTERNAL_DATA_B[IDX_SELECT];
+                    12: DATA_IN = EXTERNAL_DATA_C[IDX_SELECT];
+                    13: DATA_IN = EXTERNAL_DATA_D[IDX_SELECT];
+                    14: DATA_IN = EXTERNAL_DATA_E[IDX_SELECT];
+                    15: DATA_IN = EXTERNAL_DATA_F[IDX_SELECT];
+                endcase
 
-            #10 CLK = ~CLK;
-            #10 CLK = ~CLK;
+                #10 CLK = ~CLK;
+                #10 CLK = ~CLK;
+            end     
         end
-
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-
-        #10
-
-        $display("Writing (WEIGHTS) to Memory...");
-        WRITE = 1;
-        LOAD = 0;
-
-        // Elements
-        for(integer i = 0; i < 64; i = i + 1) begin
-            DATA_IN = (i[15:0] % 8) + 1;
-            ADDR = i[7:0] + 8'd64;
-
-            $display("DATA_IN: %d | ADDR: %d", DATA_IN, ADDR);
-
-            #10 CLK = ~CLK;
-            #10 CLK = ~CLK;
-        end
-
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-
-        #10
-
-
-        $display("Reading from memory...");
-
-        WRITE = 1;
-        LOAD = 1;
-        for(integer i = 1; i < 64; i = i + 1) begin
-            // DATA_IN = i[15:0];
-            ADDR = i[7:0] - 1'b1;
-
-            #10 CLK = ~CLK;
-            #10 CLK = ~CLK;
-        end
-
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10;
-
-
-        $display("Loading to registers...");
-
-        WRITE = 0;
-        LOAD = 1;
-        for(integer i = 1; i < 64; i = i + 1) begin
-            // DATA_IN = i[15:0];
-            ADDR = i[7:0] - 1'b1;
-            
-
-            #10 CLK = ~CLK;
-            #10 CLK = ~CLK;
-        end
-
-
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10;
         
 
-        $display("MATRIX MULTIPLLICATION BABY!!!");
-        WRITE = 0;
-        LOAD = 0;
-        for(integer i = 1; i < 64; i = i + 1) begin
-            // DATA_IN = i[15:0];
-            // ADDR = i[7:0] - 1'b1;
-
+        #50;
+        
+        $display("Long delay test...");
+        for(integer i = 0; i < 50; i = i + 1) begin
             #10 CLK = ~CLK;
             #10 CLK = ~CLK;
         end
 
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10 CLK = ~CLK;
-        #10;
+
+        $display("Loading to REGISTER FILE...");
+        WRITE = 0;
+        LOAD = 1;
+        
+        for(integer i = 0; i < 50; i = i + 1) begin
+            #10 CLK = ~CLK;
+            #10 CLK = ~CLK;
+        end
+
+        #50;
+
+        $display("Doing MATMUL...");
+        WRITE = 0;
+        LOAD = 0;
+        for(integer i = 0; i < 50; i = i + 1) begin
+            #10 CLK = ~CLK;
+            #10 CLK = ~CLK;
+        end
+
+        #50;
+
+        // $display("Reading Results...");
+        // WRITE = 1;
+        // LOAD = 1;
+        // for(integer i = 0; i < 50; i = i + 1) begin
+        //     #10 CLK = ~CLK;
+        //     #10 CLK = ~CLK;
+        // end
 
 
         $display("BYE!");
@@ -132,9 +167,15 @@ module top_module_tb();
         .CLK(CLK),
         .EN(EN),
         .WRITE(WRITE),
+        
         .LOAD(LOAD),
-        .ADDR(ADDR),
+
+
+        .IDX(IDX_SELECT),
+        .REG(REG_SELECT),
         .DATA_IN(DATA_IN),
+
+
         .DATA_OUT(DATA_OUT)
     );
 
